c4aadf93f39b5679f466565db78c31d0f3f610b1
Philliphigo <144315556+Philliphigo@users.noreply.github.com>
2024-11-19 16:44:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Sample product data
 const products = [
-  { id: 1, name: "T-Shirt", price: 5000, img: "tshirt.jpg" },
+  { id: 1, name: "T-Shirt", price: 5000, img: "copilot_image_1731973074719.jpeg" },
   { id: 2, name: "Hoodie", price: 10000, img: "hoodie.jpg" },
   { id: 3, name: "Cap", price: 3000, img: "cap.jpg" },
 ];
